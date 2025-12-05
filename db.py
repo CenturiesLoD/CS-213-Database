c8718cbec20b9a5a1d98a6195a615fd28db01f84
CenturiesLoD <pc3316@nyu.edu>
2025-12-05 18:59:03 +0800

    env for db.py

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 import pymysql
-
+import os
+from dotenv import load_dotenv 
+load_dotenv()
 def get_db_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="123456",  # 你刚设置的密码
-        database="air_reservation",
-        charset="utf8mb4",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),  # 你刚设置的密码
+        database=os.getenv("DB_NAME"),
+        charset=os.getenv("DB_CHARSET"),
         cursorclass=pymysql.cursors.DictCursor
     )
